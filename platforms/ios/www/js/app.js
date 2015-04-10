c84d547b56b7bb3e8fe5d65facf507f554976518
lisa <lissdy2013@gmail.com>
2015-04-10 20:46:49 +0800

    项目管理

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,65 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-/*angular.module('starter', ['ionic'])
-angular.module('todo', ['ionic'])
-
-.controller('TodoCtrl', function($scope, $ionicModal) {
-  // No need for testing data anymore
-  $scope.tasks = [];
-
-  // Create and load the Modal
-  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
-    $scope.taskModal = modal;
-  }, {
-    scope: $scope,
-    animation: 'slide-in-up'
-  });
-
-  // Called when the form is submitted
-  $scope.createTask = function(task) {
-    $scope.tasks.push({
-      title: task.title
-    });
-    $scope.taskModal.hide();
-    task.title = "";
-  };
-
-  // Open our new task modal
-  $scope.newTask = function() {
-    $scope.taskModal.show();
-  };
-
-  // Close the new task modal
-  $scope.closeNewTask = function() {
-    $scope.taskModal.hide();
-  };
-});*/
-/*.controller('TodoCtrl', function($scope) {
-  $scope.tasks = [
-    { title: 'Collect coins' },
-    { title: 'Eat mushrooms' },
-    { title: 'Get high enough to grab the flag' },
-    { title: 'Find the Princess' }
-  ];
-});*/
-
-/*.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})*/
-
-
 angular.module('todo', ['ionic'])
 /**
  * The Projects factory handles saving and loading projects
@@ -69,14 +7,14 @@ angular.module('todo', ['ionic'])
 .factory('Projects', function() {
   return {
     all: function() {
-      var projectString = window.localStorage['projects'];
+      var projectString = window.localStorage['project'];
       if(projectString) {
         return angular.fromJson(projectString);
       }
       return [];
     },
     save: function(projects) {
-      window.localStorage['projects'] = angular.toJson(projects);
+      window.localStorage['project'] = angular.toJson(projects);
     },
     newProject: function(projectTitle) {
       // Add a new project
@@ -114,7 +52,7 @@ angular.module('todo', ['ionic'])
 
   // Called to create a new project
   $scope.newProject = function() {
-    var projectTitle = prompt('Project name');
+    var projectTitle = prompt('项目名称');
     if(projectTitle) {
       createProject(projectTitle);
     }
@@ -136,13 +74,14 @@ angular.module('todo', ['ionic'])
 
   $scope.createTask = function(task) {
     if(!$scope.activeProject || !task) {
+      alert(1)
       return;
     }
     $scope.activeProject.tasks.push({
       title: task.title
     });
     $scope.taskModal.hide();
-
+    alert(tasks)
     // Inefficient, but save all the projects
     Projects.save($scope.projects);
 
@@ -168,7 +107,7 @@ angular.module('todo', ['ionic'])
   $timeout(function() {
     if($scope.projects.length == 0) {
       while(true) {
-        var projectTitle = prompt('Your first project title:');
+        var projectTitle = prompt('请输入第一个项目名称：');
         if(projectTitle) {
           createProject(projectTitle);
           break;
